457dfe4b5c9206c6b9eaf8521fe83bce77d9442b
MatsWallden <mats@skillsta.com>
2026-04-23 16:04:36 +0200

    adding data_source and key_feature to configuration

---
+++ src/qualified/configuration/data_source.py
@@ -0,0 +1,6 @@
+"""module contains enumeration DataSource"""
+from enum import Enum
+
+class DataSource(Enum):
+    """enumerates DataSource"""
+    TEST=0
---
+++ src/qualified/configuration/key_feature.py
@@ -0,0 +1,6 @@
+"""module contains enumeration KeyFeature"""
+from enum import Enum
+
+class KeyFeature(Enum):
+    """enumerates KeyFeatures"""
+    TEST=0
